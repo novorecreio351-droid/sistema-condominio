150a7c814e034743f1a4ddc201d67bd0ca09ac23
novorecreio351-droid <novorecreio351@gmail.com>
2026-02-14 03:46:41 -0300

    Descrição da mudança

--- src/pages/Churrasqueira.jsx
+++ src/pages/Churrasqueira.jsx
@@ -1795,7 +1795,7 @@ const itensExibidos = React.useMemo(() => {
     <input
     type="file"
     multiple
-    accept="image/*,application/pdf"
+    accept="image/*, .pdf, application/pdf"
     onChange={handleFileChange}
     style={{ display: 'none' }}
     id="file-upload"  /* O id deve ser exatamente igual ao htmlFor do label */
