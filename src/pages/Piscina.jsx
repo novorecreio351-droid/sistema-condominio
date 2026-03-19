58145f18c465aaffd9bb06e8c653328949f40585
novorecreio351-droid <novorecreio351@gmail.com>
2026-03-19 17:48:12 -0300

    Descrição da mudança

--- src/pages/Piscina.jsx
+++ src/pages/Piscina.jsx
@@ -522,9 +522,9 @@ const handleSave = async () => {
       f.status === "Ativo"
     );
 
-    if (ativosNaUnidade.length >= 7) {
+    if (ativosNaUnidade.length >= 8) {
       alert(
-        `Não é possível cadastrar. A unidade ${formData.id_unidade} já atingiu o limite de 5 moradores com status 'Ativo'.\n\n` +
+        `Não é possível cadastrar. A unidade ${formData.id_unidade} já atingiu o limite de 8 moradores com status 'Ativo'.\n\n` +
         `Para cadastrar um novo, altere o status de um morador antigo para 'Inativo'.`
       );
       return; // Interrompe o salvamento
